0298f2d0b8caaa9ef61fbc3fe785349b823e393d
Yang jeongwoo <25_39@bssm.hs.kr>
2026-04-09 08:19:31 +0900

    Kotlin에서의 형변환

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,7 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    var longString = """이 문자열은
-        매우 길어서
-        따옴표를
-        세 개를 사용해야
-        합니다.
-    """.trimMargin()
+    var beInt = "123"
+    var realInt = beInt.toInt()
 
-    println(longString)
+    println(beInt::class.simpleName)
+    println(realInt::class.simpleName)
 }
