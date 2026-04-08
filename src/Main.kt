8fa85667fea608665395777eb969cbded9601079
Yang jeongwoo <25_39@bssm.hs.kr>
2026-04-09 08:15:13 +0900

    따옴표 세 개로 문자열 줄바꿈

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,12 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    var nullable: String? = null
+    var longString = """이 문자열은
+        매우 길어서
+        따옴표를
+        세 개를 사용해야
+        합니다.
+    """.trimMargin()
 
-    println(nullable)
+    println(longString)
 }
