58eb15302f45a8254e8b7af4352be814907d56a9
Yang jeongwoo <25_39@bssm.hs.kr>
2026-04-09 23:34:22 +0900

    함수 반환형 타입 추론

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,6 @@
 fun main() {
     var arr: Array<Int> = arrayOf(1,2,3)
-    println(sum(arr))
+    println(sumArr(arr))
 }
 
-fun sum(arr: Array<Int>): Int {
-    var sum = 0
-    for (i in arr.indices) {
-        sum += arr[i]
-    }
-    return sum
-}
+fun sumArr(arr: Array<Int>) = arr.sum()
