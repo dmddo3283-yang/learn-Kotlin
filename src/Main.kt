6df81b95c36e1af4a79056b39ca2b7f04c0905fb
Yang jeongwoo <25_39@bssm.hs.kr>
2026-04-10 10:32:02 +0900

    when 구문 사용

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,12 @@
 fun main() {
-    var arr: Array<Int> = arrayOf(1,2,3)
-    println(arr is Array<Int>)
+    doWhen("양정우")
+}
+
+fun doWhen(a: Any) {
+    when(a) {
+        is Int -> println("$a: 정수형입니다")
+        is Double -> println("$a: 실수형입니다")
+        is Long -> println("$a: 와우 길다")
+        is Any -> println("$a: 정우입니다")
+    }
 }
