5b30c2a104d514609fb39a2ba943e45a44503aef
Yang jeongwoo <25_39@bssm.hs.kr>
2026-04-10 11:04:04 +0900

    반복문 step

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,5 @@
 fun main() {
-    doWhen("양정우")
-}
-
-fun doWhen(a: Any) {
-    when(a) {
-        is Int -> println("$a: 정수형입니다")
-        is Double -> println("$a: 실수형입니다")
-        is Long -> println("$a: 와우 길다")
-        is Any -> println("$a: 정우입니다")
+    for (i in 1..10 step 2) {
+        println(i)
     }
 }
