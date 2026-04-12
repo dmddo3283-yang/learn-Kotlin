9abf18cce31cc905dac568dc979274209d525df7
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-04-13 08:17:20 +0900

    add printMe method to Person class

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,4 @@
 fun main() {
     var person1 = Person("양정우", 18);
-    println("안녕하세요, 저는 ${person1.name}이고, 나이는 ${person1.age}세입니다!")
+    person1.printMe()
 }
--- src/Person.kt
+++ src/Person.kt
@@ -1,3 +1,5 @@
 class Person (var name: String, var age: Int){
-
+    fun printMe() {
+        println("안녕하세요, 저는 ${name}이고, 나이는 ${age}세입니다!")
+    }
 }
