1d19e7a378a9ec1f00517f27bef250fc7819c01b
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-04-13 08:19:48 +0900

    add init method to Person class

--- src/Person.kt
+++ src/Person.kt
@@ -1,4 +1,8 @@
 class Person (var name: String, var age: Int){
+    init {
+        println("Person Class Created")
+    }
+
     fun printMe() {
         println("안녕하세요, 저는 ${name}이고, 나이는 ${age}세입니다!")
     }
