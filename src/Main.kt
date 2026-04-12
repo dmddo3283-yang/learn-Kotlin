f39ef20ef5ab89601e78676d7db73440508d9c75
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-04-13 08:24:28 +0900

    create secondary constructor to Person class

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,6 @@
 fun main() {
-    var person1 = Person("양정우", 18);
+    var person1 = Person("양정우", 18)
     person1.printMe()
+    var person2 = Person("김진용")
+    person2.printMe()
 }
--- src/Person.kt
+++ src/Person.kt
@@ -3,6 +3,8 @@ class Person (var name: String, var age: Int){
         println("Person Class Created")
     }
 
+    constructor(name: String) : this(name, 18);
+
     fun printMe() {
         println("안녕하세요, 저는 ${name}이고, 나이는 ${age}세입니다!")
     }
